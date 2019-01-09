d3cfbc2bf76d0c7c62b2ad98bf7d793df3436b54
wdziurdzia <wdziurdzia@sigma.ug.edu.pl>
2019-01-09 11:11:27 +0100

    Add files via upload

---
+++ Projekt1/Projekt1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1
+{
+    public class Test
+    {
+        int x { get; set; }
+        public int daj(int y)
+        {
+            x = y + 5;
+            return x;
+        }
+
+    }
+    public class TestDane
+    {
+        string imie;
+
+        public string poloncz(string nazwisko)
+        {
+            imie = "Anna";
+            return nazwisko + imie;
+        }
+    }
+
+    public class TestCollection
+    {
+        public List<int> actual = new List<int>();
+        public TestCollection()
+        {
+            actual.Add(10);
+            actual.Add(20);
+            actual.Add(30);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Projekt1/Projekt1/Projekt1Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Projekt1
+{
+    public class Projekt1Tests
+    {
+        [TestFixture]
+        public class UnitTest1
+        {
+            private TestCollection Test;
+
+            [Test]
+            public void TestMethod1()
+            {
+                Test topkek = new Test();
+
+                Assert.AreEqual(topkek.daj(4), 9);
+            }
+            [Test]
+            public void TestString()
+            {
+                TestDane toplel = new TestDane();
+                StringAssert.Contains(toplel.poloncz("Kowalska"), "KowalskaAnna");
+            }
+            [Test]
+            public void TestCollection()
+            {
+                Test = new TestCollection();
+                System.Collections.Generic.List<int> expected = new System.Collections.Generic.List<int>();
+                expected.Add(10);
+                expected.Add(20);
+                expected.Add(30);
+
+                CollectionAssert.AreEqual(Test.actual, expected);
+            }
+        }
+    }
+}
